00d91c398095b82fa73d203556360557822ef35c
Shohjahonqwer <132754946+Shohjahonqwer@users.noreply.github.com>
2023-08-01 18:51:27 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// 2 masala
+
+
+let b = Math.floor(Math.random() * 1000000)
+
+function Chek(number) {
+
+    let a = number.toString();
+    
+    let first = a.slice(0, 3);
+    let last = a.slice(3);
+    
+
+    if (first === last) {
+
+      alert("YES");
+
+    } else {
+
+      alert("NO");
+
+    }
+}
+
+
+
+Chek(b)
+
+
+  
+
+
+
+  
